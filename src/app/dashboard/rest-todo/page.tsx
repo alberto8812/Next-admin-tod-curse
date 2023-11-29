0b941935e323f2904a51676a38aad8b4cec81321
alberto8812 <cvelascosaavedra@gmail.com>
2023-11-28 21:34:07 -0500

    use revalidate

--- src/app/dashboard/rest-todo/page.tsx
+++ src/app/dashboard/rest-todo/page.tsx
@@ -1,3 +1,6 @@
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
 import prisma from "@/app/lib/prima";
 import { TodosGrid } from "@/todos";
 import { NewTodo } from "@/todos/components/NexTodo";
--- src/todos/components/TodoItem.tsx
+++ src/todos/components/TodoItem.tsx
@@ -1,9 +1,10 @@
 'use client'
 import { Todo } from "@prisma/client"
-import { FC } from "react"
+import { FC, startTransition } from "react"
 
 import styles from "./TodoItem.module.css";
 import {  IoCheckboxOutline, IoSquareOutline } from "react-icons/io5";
+import { useOptimistic } from "react";
 
 
 
@@ -16,21 +17,44 @@ interface Props{
 
 
 export const TodoItem:FC <Props> = ({todo,toggleTodo}) => {
+ 
+ /**
+  * funcion como un use state
+  */
+  const [todoOptimistic,toggleOptimistic]=useOptimistic(
+    todo,
+    /**
+     * segundo argumento llamamos un callback
+     */
+    (state,newCompleteValue:boolean)=>({...state,complete:newCompleteValue})
+    );
+
+    const onToggleTdo=async()=>{
+      try {
+        startTransition(()=> toggleOptimistic(!todoOptimistic.complete))
+        await toggleTodo(todoOptimistic.id,!todoOptimistic.complete)
+      } catch (error) {
+        startTransition(()=> toggleOptimistic(!todoOptimistic.complete))
+      }
+    }
+
+
+
   return (
     <div className={todo.complete?styles.todoDone:styles.todoPending}>
         <div className="flex flex-col sm:flex-row justify-start items-center gap-4">
           <button
-            onClick={()=>toggleTodo(todo.id,todo.complete?false:true)}
+            onClick={()=>onToggleTdo()}
           >
             <div className={`
                 flex p-2 
                 rounded-md 
                 cursor-pointer 
                 hover:bg-opacity-60 
-              ${todo.complete?'bg-blue-100':'bg-red-100'}
+              ${todoOptimistic.complete?'bg-blue-100':'bg-red-100'}
             `}>
                 {
-                    todo.complete?
+                    todoOptimistic.complete?
                     <IoCheckboxOutline size={30}/>
                     : <IoSquareOutline size={30}/>                    
                 }
@@ -38,7 +62,7 @@ export const TodoItem:FC <Props> = ({todo,toggleTodo}) => {
           </button>
             <div className="text-center sm:text-left">
                 {
-                    todo.description
+                    todoOptimistic.description
                 }
             </div>
         </div>
